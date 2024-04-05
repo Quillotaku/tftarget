656de3fbb714cd87f65c83e50bb382cd22eecf1a
Ionut Alexandru <petrin.ionut@evolutio.com>
2024-04-05 12:02:36 +0200

    add mode option

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # tftarget
 
-Uso: python3 target.py [help|[dir|file]]<br>
-dir: directorio donde se encuentran los tf para sacar todos los recursos<br>
-file: archivo de tf para sacar los recursos<br>
-mode: modo de ejecución de terraform<br>
-- plan<br>
-- apply<br>
-- destroy<br>
+Uso: python3 target.py [help|[dir|file]|mode|var]<br>
+Indicar al menos `dir` o `file`.<br>
+dir: Path del directorio donde se encuentran los tf para sacar todos los recursos. OBLIGATORIO<br>
+file: Path del archivo de tf para sacar los recursos. OBLIGATORIO.<br>
+mode: Modo de ejecución de terraform. OPCIONAL<br>
+- plan
+- apply
+- destroy
+
+var: Path de las variables para terraform, se puede repetir tantas veces como sea necesario. OPCIONAL.<br>
 
 Ejemplos:<br>
-- python3 target.py dir /home/ubuntu/tf<br>
-- python3 target.py file /home/ubuntu/tf/main.tf<br>
-- python3 target.py dir /home/ubuntu/tf mode plan<br>
-- python3 target.py file /home/ubuntu/tf/main.tf mode apply<br>
-- python3 target.py help<br>
+- python3 target.py dir /home/ubuntu/tf
+- python3 target.py file /home/ubuntu/tf/main.tf
+- python3 target.py dir /home/ubuntu/tf mode plan
+- python3 target.py file /home/ubuntu/tf/main.tf mode apply
+- python3 target.py dir /home/ubuntu/tf -var /home/ubuntu/tf/variables.tfvars
+- python3 target.py file /home/ubuntu/tf/main.tf -var /home/ubuntu/tf/variables.tfvars
+- python3 target.py dir /home/ubuntu/tf mode plan -var /home/ubuntu/tf/variables.tfvars
+- python3 target.py file /home/ubuntu/tf/main.tf mode apply -var /home/ubuntu/tf/variables.tfvars
+- python3 target.py file /home/ubuntu/tf/main.tf -var /home/ubuntu/tf/variables.tfvars -var /home/ubuntu/tf/vars.tfvars
+- python3 target.py dir /home/ubuntu/tf mode plan -var /home/ubuntu/tf/variables.tfvars -var /home/ubuntu/tf/vars.tfvars
+- python3 target.py help
--- target.py
+++ target.py
@@ -1,27 +1,34 @@
 import os
-import json
 import sys
 
 def help():
     print("""
-Uso: python3 target.py [help|[dir|file]|mode]
-dir: directorio donde se encuentran los tf para sacar todos los recursos
-file: archivo de tf para sacar los recursos
-mode: modo de ejecución de terraform
+Uso: python3 target.py [help|[dir|file]|mode|var]
+Indicar al menos dir o file.
+dir: Path del directorio donde se encuentran los tf para sacar todos los recursos. OBLIGATORIO.
+file: Path del archivo de tf para sacar los recursos. OBLIGATORIO.
+mode: Modo de ejecución de terraform. OPCIONAL.
           - plan
           - apply
           - destroy
+var: Path de las variables para terraform, se puede repetir tantas veces como sea necesario. OPCIONAL.
 Ejemplos:
           - python3 target.py dir /home/ubuntu/tf
           - python3 target.py file /home/ubuntu/tf/main.tf
           - python3 target.py dir /home/ubuntu/tf mode plan
           - python3 target.py file /home/ubuntu/tf/main.tf mode apply
+          - python3 target.py dir /home/ubuntu/tf -var /home/ubuntu/tf/variables.tfvars
+          - python3 target.py file /home/ubuntu/tf/main.tf -var /home/ubuntu/tf/variables.tfvars
+          - python3 target.py dir /home/ubuntu/tf mode plan -var /home/ubuntu/tf/variables.tfvars
+          - python3 target.py file /home/ubuntu/tf/main.tf mode apply -var /home/ubuntu/tf/variables.tfvars
+          - python3 target.py file /home/ubuntu/tf/main.tf -var /home/ubuntu/tf/variables.tfvars -var /home/ubuntu/tf/vars.tfvars
+          - python3 target.py dir /home/ubuntu/tf mode plan -var /home/ubuntu/tf/variables.tfvars -var /home/ubuntu/tf/vars.tfvars
           - python3 target.py help
 """)
     sys.exit(0)
 
 def init_params():
-    params = {"dir":"", "mode":"", "help":"","file":""}
+    params = {"dir":"", "mode":"", "help":"","file":"","var":""}
     return params
 
 def get_params(params):
@@ -33,13 +40,15 @@ def get_params(params):
             par[params[i]] = params[i+1]
         elif params[i] == "mode":
             par[params[i]] = params[i+1]
+        elif params[i] == "var":
+            par[params[i]] += params[i+1]+";"
         elif params[i] == "help":
             help()
     return par
 
 def get_tf_files(dir):
     tf = []
-    for root, dirs, files in os.walk(dir):
+    for root, dirs, files in os.walk(dir): #ignorar dirs, necesario para acceder a files
         for file in files:
             if root == dir:
                 if file.endswith(".tf"):
@@ -63,7 +72,11 @@ def get_data(tf):
                     res.append(data)
     return res
 
-def generate_targets(res,mode):
+def generate_targets(res,mode,var):
+    varfile = ""
+    for v in var:
+        print(v)
+        varfile += " -var-file="+v
     for i in range(1,len(res)):
         print("%3d. %s " % (i,res[i]))
     targets = input("Cuales quieres para el target? (lista separada por espacios): ")
@@ -75,9 +88,9 @@ def generate_targets(res,mode):
         except:
             print("No existe el target %d, se omite." % int(target))
     if mode == "":
-        print(" -target ".join(tg))
+        print(" -target ".join(tg)+varfile)
     else:
-        print("terraform "+mode+" -target ".join(tg))
+        print("terraform "+mode+" -target ".join(tg)+varfile)
 
 
 if __name__ == "__main__":
@@ -90,6 +103,6 @@ if __name__ == "__main__":
         resources = get_data(tf=tf_files)
     if params["file"] != "":
         resources = get_data(tf=[params["file"]])
-    generate_targets(res=resources,mode=params["mode"])
+    generate_targets(res=resources,mode=params["mode"],var=params["var"].split(";")[0:-1])
     exit()
 
